bd3364931ae122f1790aabe5a6b294e7e5edb3cc
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-13 12:27:43 -0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname} ⁝⁝ ${uptime} ⌛ ${lenguajeGB.lenguaje() == 'es' ? '.menu2' : '#status #menu #jadibot #groupsgb #owner'} 💻` 
+let bio = `${global.packname} 🥇 ${uptime} ⌛ ${lenguajeGB.lenguaje() == 'es' ? '.menu2' : '#status #menu #jadibot #groupsgb #owner'} 💻` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
