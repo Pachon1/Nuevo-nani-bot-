2095cc35fc68ef8134f114a8fd072f03846ccf1a
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-13 18:03:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -165,7 +165,7 @@ global.img21 = 'https://i.imgur.com/YfK3oTD.jpg' //paypal
 global.welgata = [ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb]
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18, img19, img20, img21]
 global.gataMenu2 = [imagen14]
-global.gataMenu3 = [menu3]
+
 global.gatadonar = [imagen15]
 global.gatahot = [imagen16, imagen17, imagen18]
 global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13]
