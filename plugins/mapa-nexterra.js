a96328104666901f7e9d126e5babab17fe16a868
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-13 15:32:56 -0500

    Update mapa-nexterra.js

--- plugins/mapa-nexterra.js
+++ plugins/mapa-nexterra.js
@@ -2,7 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://telegra.ph/file/72a80fdd61b735ae9a6db.png' 
+let img = 'https://telegra.ph/file/8298993334725ab608d77.jpg' 
 let texto = `» 𝙈𝘼𝙋𝘼 𝙉𝙀𝙓𝙏𝙀𝙍𝙍𝘼 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ✅`
 
 const fkontak = {
